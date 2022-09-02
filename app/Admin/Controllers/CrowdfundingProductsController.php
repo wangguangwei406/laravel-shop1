3abe4d9d56b4076d34a61b6ac81f12256a448933
paul <690438605@qq.com>
2022-09-02 14:41:33 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -9,9 +9,14 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 
-class CrowdfundingProductsController extends AdminController
+class CrowdfundingProductsController extends CommonProductsController
 {
     protected $title = '众筹商品';
+
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
 
     protected function grid()
     {
@@ -76,5 +81,28 @@ class CrowdfundingProductsController extends AdminController
         });
 
         return $form;
+    }
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', ' 状态')->display(function ($value) {
+            return CrowdfundingProduct::$statusMap[$value];
+        });
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 众筹相关字段
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
     }
 }
